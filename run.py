1c5b6eee29b2fc5d71a240b19fd1a313aaa40f27
nabin <nabin2055karki@gmail.com>
2022-06-26 17:24:44 +0545

    Inital Commit For script

---
+++ run.py
@@ -0,0 +1,24 @@
+
+from bs4 import BeautifulSoup
+import requests
+import csv
+import os
+
+source = requests.get('https://cdn.jsdelivr.net/gh/younginnovations/internship-challenges@master/data-analysis/scrape-it/exampledata.html').text
+soup = BeautifulSoup(source, 'lxml')
+
+#print(soup.prettify())
+
+os.mkdir('out')
+csv_file = open('out/'+'data.csv', 'w', newline='')
+csv_writer = csv.writer(csv_file,delimiter=",")
+new_list=[]
+for row in soup.find_all('tr'):
+  local_list=[]
+  for t_data in row.find_all('td'):
+      local_list.append(t_data.text)
+  new_list.append(local_list)    
+
+for item in new_list:
+  csv_writer.writerow(item)
+csv_file.close()
