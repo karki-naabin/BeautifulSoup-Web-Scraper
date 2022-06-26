41c9535665899c2d66a03b1c94b34961eb95d2c5
nabin <nabin2055karki@gmail.com>
2022-06-26 17:27:24 +0545

    Add Comments For Code Description
    This commit adds necessary descriptions to various codes through comments

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
-
+#importing necessary modules and libraires
 from bs4 import BeautifulSoup
 import requests
 import csv
 import os
 
+#using requests to fetch source code and using the lxml parser
 source = requests.get('https://cdn.jsdelivr.net/gh/younginnovations/internship-challenges@master/data-analysis/scrape-it/exampledata.html').text
 soup = BeautifulSoup(source, 'lxml')
 
 #print(soup.prettify())
 
+#creating folder, csv file and parsing html rows to csv file
 os.mkdir('out')
 csv_file = open('out/'+'data.csv', 'w', newline='')
 csv_writer = csv.writer(csv_file,delimiter=",")
